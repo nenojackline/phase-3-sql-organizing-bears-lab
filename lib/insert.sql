f13e9a158b2dffea72c78fde210c9d7b5e2bf1bb
jackline chepngeno <chepngenojackline0@gmail.com>
2022-08-21 10:25:22 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1,"Mr. Chocolate",2,"M","grey","angry",0);
+INSERT INTO bears VALUES(2,"Rowdy",1,"F","black","happy",1);
+INSERT INTO bears VALUES(3,"Tabitha",6,"M","brown","nice",0);
+INSERT INTO bears VALUES(4,"Sargeant Brown",9,"M","Green","jovial",1);
+INSERT INTO bears VALUES(5,"Melissa",3,"F","dark brown","goofy",0);
+INSERT INTO bears VALUES(6,"Grinch",2,"F","pink","nice",1);
+INSERT INTO bears VALUES(7,"Wendy",10,"M","white","naive",0);
+INSERT INTO bears VALUES(8,null,14,"F","black","happy",1);
